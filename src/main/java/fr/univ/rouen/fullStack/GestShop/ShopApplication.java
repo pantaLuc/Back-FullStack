e0f788c454ba5db27f41ff633826396c18cf107c
ichraq elaidi <ichraqelaidi55@gmail.com>
2022-12-07 21:37:30 +0100

    first essential devlop

--- src/main/java/fr/univ/rouen/fullStack/GestShop/ShopApplication.java
+++ src/main/java/fr/univ/rouen/fullStack/GestShop/ShopApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestShopApplication {
+public class ShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestShopApplication.class, args);
+		SpringApplication.run(ShopApplication.class, args);
 	}
 
 }
+
