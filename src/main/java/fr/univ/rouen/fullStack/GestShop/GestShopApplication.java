b48ab52d79deda6e08bf06f2164b406be22c69c4
ichraq elaidi <ichraqelaidi55@gmail.com>
2022-12-07 21:23:40 +0100

    first commit

---
+++ src/main/java/fr/univ/rouen/fullStack/GestShop/GestShopApplication.java
@@ -0,0 +1,13 @@
+package fr.univ.rouen.fullStack.GestShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/univ/rouen/fullStack/GestShop/GestShopApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univ.rouen.fullStack.GestShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
