b44a34366fe51ac4e2e14bf5bac327abb44b92f7
ichraq elaidi <ichraqelaidi55@gmail.com>
2022-12-28 15:56:29 +0100

    search boutiques

--- src/main/java/fr/univ/rouen/fullStack/GestShop/repository/BoutiqueRepository.java
+++ src/main/java/fr/univ/rouen/fullStack/GestShop/repository/BoutiqueRepository.java
@@ -1,7 +1,9 @@
 package fr.univ.rouen.fullStack.GestShop.repository;
 
 import fr.univ.rouen.fullStack.GestShop.models.Boutique;
+import fr.univ.rouen.fullStack.GestShop.models.Utilisateur;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -9,7 +11,9 @@ import org.springframework.data.repository.CrudRepository;
 
 
 public interface BoutiqueRepository extends CrudRepository<Boutique, Long> {
-    Optional<Boutique> findByNom(String nom);
+	Optional<Boutique> findByNom(String nom);
+	
+	List<Boutique> findByUtilisateur(Utilisateur utilisateur);
 
 
 }
