9da4d1d0b09b3480b2541bbe77c4003ddb09d071
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-12-29 10:44:41 +0100

    readding getter and setter

--- src/main/java/fr/univ/rouen/fullStack/GestShop/models/Boutique.java
+++ src/main/java/fr/univ/rouen/fullStack/GestShop/models/Boutique.java
@@ -12,8 +12,6 @@ import java.util.*;
 import java.util.List;
 
 @Entity
-@Getter
-@Setter
 public class Boutique {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -46,4 +44,51 @@ public class Boutique {
 		this.utilisateur = utilisateur;
 	}
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public LocalDateTime getDateCreationBoutique() {
+		return dateCreationBoutique;
+	}
+
+	public void setDateCreationBoutique(LocalDateTime dateCreationBoutique) {
+		this.dateCreationBoutique = dateCreationBoutique;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public List<Horaire> getHoraireList() {
+		return horaireList;
+	}
+
+	public void setHoraireList(List<Horaire> horaireList) {
+		this.horaireList = horaireList;
+	}
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
 }
